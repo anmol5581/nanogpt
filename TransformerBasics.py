79480e4ef758d7c601358eb6243194e0ba7cbcbf
anmol5581 <anmol5581@gmail.com>
2026-08-08 12:06:51 +0530

    Initial NanoGPT project

---
+++ TransformerBasics.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+
+batch_size = 32
+block_size = 8
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
+eval_iters = 200
+n_embed = 32
+
+torch.manual_seed(1331)
+
+with open('/Users/anmolagrawal/Documents/Transformer/input.txt','r',encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos = {i:ch for i,ch in enumerate(chars)}
+
+encode = lambda s: [stoi[ch] for ch in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(text),dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size,(batch_size,))
+    x = torch.stack([data[i:i+block_size]for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1]for i in ix])
+    return x,y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(nn.Module):
+
+    def __init__(self,head_size) -> None:
+        super().__init__()
+        self.key = nn.Linear(n_embed,head_size, bias=False)
+        self.query = nn.Linear(n_embed,head_size, bias=False)
+        self.value = nn.Linear(n_embed,head_size, bias=False)
+        self.tril = torch.tril(torch.ones(block_size,block_size))
+
+    def forward(self,x):
+        B,T,C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        wei = q@k.transpose(-2,-1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T]==0,float('-inf'))
+        wei = F.softmax(wei,dim=-1)
+        v = self.value(x)
+        out = wei@v
+        return out
+    
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self,num_heads,head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embed,n_embed)
+
+    def forward(self,x):
+        out = torch.cat([h(x) for h in self.heads],dim=-1)
+        out = self.proj(out)
+        return out
+    
+
+class FeedForward(nn.Module):
+
+    def __init__(self,n_embed) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed,4*n_embed),
+            nn.ReLU(),
+            nn.Linear(4*n_embed,n_embed)
+        )
+
+    def forward(self,x):
+        return self.net(x)
+    
+class Block(nn.Module):
+
+    def __init__(self, n_embed,n_head) -> None:
+        super().__init__()
+        head_size = n_embed//n_head
+        self.sa = MultiHeadAttention(n_head,head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self,x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+        
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size,n_embed)
+        self.positional_embedding = nn.Embedding(block_size,n_embed)
+        self.blocks = nn.Sequential(
+            Block(n_embed,n_head=4),
+            Block(n_embed,n_head=4),
+            Block(n_embed,n_head=4),
+            Block(n_embed,n_head=4),
+            nn.LayerNorm(n_embed),
+        )
+        self.lm_head = nn.Linear(n_embed,vocab_size)
+
+    def forward(self,idx,targets=None):
+        B, T = idx.shape
+        tok_embed = self.token_embedding(idx)
+        pos_embed = self.positional_embedding(torch.arange(T))
+        x = tok_embed + pos_embed
+        x = self.blocks(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B,T,C = logits.shape
+            logits = logits.view(B*T,C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits,targets)
+        return logits,loss
+    
+    def generate(self,idx,max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
+            logits,loss = self(idx_cond)
+            logits = logits[:,-1,:]
+            probs = F.softmax(logits,dim=-1)
+            idx_next = torch.multinomial(probs,num_samples=1)
+            idx = torch.cat((idx,idx_next),dim=1)
+        return idx
+
+model = BigramLanguageModel()
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    xb,yb = get_batch('train')
+    
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    
+    logit,loss = model(xb,yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+context = torch.zeros((1, 1), dtype=torch.long)
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
